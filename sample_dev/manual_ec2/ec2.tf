d6204427b2d18264593452f1a7e751171f5844cb
Satoshi Tanaka <tanarky@gmail.com>
2019-02-06 08:36:25 +0900

    fixed

--- sample_dev/manual_ec2/ec2.tf
+++ sample_dev/manual_ec2/ec2.tf
@@ -14,3 +14,18 @@ resource "aws_instance" "sshgw" {
   }
   count = 1
 }
+
+resource "aws_instance" "intl" {
+  instance_type = "t2.micro"
+  ami = "ami-0f63c02167ca94956"
+
+  subnet_id = "${data.terraform_remote_state.sample_dev.subnet_private_a}"
+  vpc_security_group_ids = [
+    "${data.terraform_remote_state.sample_dev.sg_allow_all_from_internal}",
+  ]
+  key_name = "${var.ec2_key_name}"
+  tags = {
+    Name = "${format("intl%02d", count.index)}"
+  }
+  count = 1
+}
